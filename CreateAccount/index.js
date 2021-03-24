28c4c456c703cf5003a7374daa13f03751235bf6
DaMews <adammews@gmail.com>
2021-03-24 17:53:32 -0300

    Aplicação do Linter Ajuste na criação da instância do repositório

--- CreateAccount/index.js
+++ CreateAccount/index.js
@@ -1,89 +1,86 @@
+import MongooseRepository from '../shared/database/repositories/mongooseRepository';
+
+import AccountService from '../shared/services/accountService';
+
+import mongoose from '../shared/database/mongoDatabase';
+
+import inputValidator from '../shared/validations/accountCreateInputValidator';
+
+import { createBaseResponse } from '../shared/http/responseUtils';
+
+import errorMessages from '../shared/constants/errorMessages';
+
+import infoMessages from '../shared/constants/infoMessages';
+
+// eslint-disable-next-line func-names
 module.exports = async function (context, req) {
-    const inputValidator = require('../shared/validations/accountCreateInputValidator');
-    const responseUtils = require('../shared/http/responseUtils');
-    const errorMessages = require('../shared/constants/errorMessages');
-    const infoMessages = require('../shared/constants/infoMessages');
+  context.log.info(`[CreateAccount] Function has been called! - ${context.invocationId}`);
 
-    const mongoose = require('../shared/database/mongoDatabase');
-    const accountService = require('../shared/services/accountService');
-    const mongooseRepository = require('../shared/database/repositories/mongooseRepository');
+  const userAccountReq = req.body || {};
 
-    require('../shared/database/models/userAccount');
+  if (Object.entries(userAccountReq).length === 0) {
+    context.log.info(`Empty body request. InvocationId: ${context.invocationId}`);
 
-    context.log.info("[CreateAccount] Function has been called! - " + context.invocationId);
+    context.res = {
+      status: 400,
+      body: createBaseResponse(false, false, errorMessages.EMPTY_REQUEST, null),
+    };
+    context.done();
 
-    const userAccountReq = req.body || {};
+    return;
+  }
 
-    if (Object.entries(userAccountReq).length === 0) {
+  const validationResult = inputValidator.createUserValidator(userAccountReq);
 
-        context.log.info("Empty body request. InvocationId: " + context.invocationId);
+  if (validationResult.errorCount !== 0) {
+    context.log.info(`Validation failed on account creation. InvocationId: ${context.invocationId}`);
 
-        context.res = {
-            status: 400,
-            body: responseUtils.createResponse(false, false, errorMessages.EMPTY_REQUEST, null)
-        }
-        context.done();
-        return;
+    const response = createBaseResponse(false, true, errorMessages.VALIDATION_ERROR_FOUND, null);
+    response.errors = validationResult.errors.errors;
 
+    context.res = {
+      status: 400,
+      body: response,
+    };
+    context.done();
+    return;
+  }
+
+  context.log.info(`Request body validated. InvocationId: ${context.invocationId}`);
+
+  try {
+    const mongoClient = await mongoose.connect(process.env.MONGO_CONNECTION, context);
+
+    const repository = new MongooseRepository(mongoClient.model('UserAccount'));
+    const service = new AccountService(repository, context);
+
+    context.log.info('Creating Account...');
+
+    const result = await service.create(userAccountReq);
+
+    if (result.error) {
+      context.log.info(`Error on creating Account: ${result.error}`, `InvocationId: ${context.invocationId}`);
+
+      context.res = {
+        status: 400,
+        body: createBaseResponse(true, false, result.error, null),
+      };
+      context.done();
+      return;
     }
 
-    let validationResult = inputValidator.createUserValidator(userAccountReq);
+    context.log.info('Success on account creation: \n', result);
 
-    if (validationResult.errorCount !== 0) {
+    context.res = {
+      status: 201,
+      body: createBaseResponse(true, false, infoMessages.SUCCESSFULLY_REGISTERED, result),
+    };
+  } catch (err) {
+    context.log.error(`An unexpected error has happened. InvocationId: ${context.invocationId}`);
 
-        context.log.info("Validation failed on account creation. InvocationId: " + context.invocationId);
-
-        let response = responseUtils.createResponse(false, true, errorMessages.VALIDATION_ERROR_FOUND, null);
-        response.errors = validationResult.errors.errors;
-
-        context.res = {
-            status: 400,
-            body: response
-        }
-        context.done();
-        return;
-    }
-
-    context.log.info("Request body validated. InvocationId: " + context.invocationId);
-
-    try {
-
-        const mongoClient = await mongoose.connect(process.env.MONGO_CONNECTION, context);
-
-        const service = new accountService(new mongooseRepository(mongoClient.model('UserAccount')), context);
-
-        context.log.info("Creating Account...");
-
-        let result = await service.create(userAccountReq);
-
-        if (result.error) {
-
-            context.log.info("Error on creating Account: " + result.error, "InvocationId: " + context.invocationId);
-
-            context.res = {
-                status: 400,
-                body: responseUtils.createResponse(true, false, result.error, null)
-            }
-            context.done();
-            return;
-
-        }
-
-        context.log.info("Success on account creation: \n", result);
-
-        context.res = {
-            status: 201,
-            body: responseUtils.createResponse(true, false, infoMessages.SUCCESSFULLY_REGISTERED, result)
-        }
-
-    } catch (err) {
-
-        context.log.error("An unexpected error has happened. InvocationId: " + context.invocationId);
-
-        context.res = {
-            status: 500,
-            body: responseUtils.createResponse(false, false, errorMessages.DEFAULT_ERROR, null)
-        }
-
-    }
+    context.res = {
+      status: 500,
+      body: createBaseResponse(false, false, errorMessages.DEFAULT_ERROR, null),
+    };
+  }
 };
--- shared/database/mongoDatabase.js
+++ shared/database/mongoDatabase.js
@@ -1,33 +1,31 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 
 let mongoClient = null;
 
 async function connect(connection, logger) {
-    logger.log("[DB] - Connecting...");
+  logger.log('[DB] - Connecting...');
 
-    mongoClient = await mongoose
-        .connect(connection, { useNewUrlParser: true, useUnifiedTopology: true })
-        .catch(error => {
-            logger.log("[DB] - Error while connecting to database", error);
-        });
+  mongoClient = await mongoose
+    .connect(connection, { useNewUrlParser: true, useUnifiedTopology: true })
+    .catch((error) => {
+      logger.log('[DB] - Error while connecting to database', error);
+    });
 
-        logger.log("[DB] - Connected");
+  logger.log('[DB] - Connected');
 
-        return mongoClient;
+  return mongoClient;
 }
 
 async function close(logger) {
-    logger.log("[DB] - Disconnecting...");
+  logger.log('[DB] - Disconnecting...');
 
-    mongoClient
-        .close()
-        .catch(error => {
-            this.logger.log("[DB] - Error while disconnecting to the database", error);
-        });
+  mongoClient
+    .close()
+    .catch((error) => {
+      this.logger.log('[DB] - Error while disconnecting to the database', error);
+    });
 
-    logger.log("[DB] - Disconnected");
+  logger.log('[DB] - Disconnected');
 }
 
-module.exports = {
-    connect, close
-}
+module.exports = { connect, close };
